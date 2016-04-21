723444351e2fd8e717dc87ee6ec7023145c07531
Ferencz Veres <ferenczv8@gmail.com>
2016-04-21 14:56:52 +0300

    added init values

--- DealershipAuto.Business/Dealership.cs
+++ DealershipAuto.Business/Dealership.cs
@@ -30,16 +30,13 @@ namespace DealershipAuto.Business
 
 			_secondHandCars = new List<ICar>();
 			_standardCars = new List<ICar>();
-			InitStandardCars();
-
 			_employees = new List<BaseEmployee>();
 
-			BaseEmployee manager = new Manager(BankAccount);
-			_employees.Add(new Employee(BankAccount) { Successor = manager });
-			_employees.Add(manager);
+			InitStandardCars();
+			InitSecondHandCars();
+			InitEmployees();
 		}
-
-
+		
 		public void BuyCar(ICar car)
 		{
 			if(car.CarTag == ECarTag.SecondHand)
@@ -113,18 +110,20 @@ namespace DealershipAuto.Business
 
 		private void InitStandardCars()
 		{
+			CarEnhancer enhancer = new CarEnhancer();
 
-			ICar car1 = new Car(123958);
+			Car car1 = new Car(123958);			
 			car1.Model = ECarModel.Nissan;
 			car1.IsClone = true;
 			car1.Price = 50000;
 			car1.Electronics.Radio = true;
 			car1.Electronics.Vitezometer = true;
 			car1.Electronics.Turometer = true;
-			
+
+			enhancer.Enhance(car1, ECarType.Basic);
 			_standardCars.Add(car1);
 
-			ICar car2 = new Car(16558);
+			Car car2 = new Car(16558);
 			car2.Model = ECarModel.Mercedes;
 			car2.IsClone = true;
 			car2.Price = 270000;
@@ -132,17 +131,58 @@ namespace DealershipAuto.Business
 			car2.Electronics.Vitezometer = true;
 			car2.Electronics.GasLevel = true;
 
+			enhancer.Enhance(car2, ECarType.Family);
 			_standardCars.Add(car2);
 
-			ICar car3 = new Car(3558);
+			Car car3 = new Car(3558);
 			car3.Model = ECarModel.Toyota;
 			car3.IsClone = true;
 			car3.Price = 80000;
-			car1.Electronics.Turometer = true;
+			car3.Electronics.Turometer = true;
 			car3.Electronics.Vitezometer = true;
 			car3.Electronics.GasLevel = true;
 
+			enhancer.Enhance(car3, ECarType.Luxury);
 			_standardCars.Add(car3);
+		}
+
+		private void InitSecondHandCars()
+		{
+			CarEnhancer enhancer = new CarEnhancer();
+
+			Car car1 = new Car(5235958);
+			car1.Model = ECarModel.Nissan;
+			car1.IsClone = true;
+			car1.Price = 30000;
+			car1.Use();
+
+			enhancer.Enhance(car1, ECarType.Basic);
+			_standardCars.Add(car1);
+
+			Car car2 = new Car(16214558);
+			car2.Model = ECarModel.Mercedes;
+			car2.IsClone = true;
+			car2.Price = 27000;
+			car2.Use();
+
+			enhancer.Enhance(car2, ECarType.Family);
+			_standardCars.Add(car2);
+
+			Car car3 = new Car(323558);
+			car3.Model = ECarModel.Toyota;
+			car3.IsClone = true;
+			car3.Price = 5000;
+			car3.Use();
+
+			enhancer.Enhance(car3, ECarType.Luxury);
+			_standardCars.Add(car3);
+		}
+
+		private void InitEmployees()
+		{
+			BaseEmployee manager = new Manager(BankAccount);
+			_employees.Add(new Employee(BankAccount) { Successor = manager });
+			_employees.Add(manager);
 		}
 
 		public bool AddStandardCar(ICar car)
