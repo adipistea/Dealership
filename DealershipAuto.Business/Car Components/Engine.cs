a153575026d03438c46e675cdc31061ae938e15c
Ferencz Veres <ferenczv8@gmail.com>
2016-04-21 14:19:52 +0300

    added Use carr method

--- DealershipAuto.Business/Car Components/Engine.cs
+++ DealershipAuto.Business/Car Components/Engine.cs
@@ -3,10 +3,10 @@
 using DealershipAuto.Business.Car_Components.Engine_Adapter;
 namespace DealershipAuto.Business.Car_Components
 {
-	public abstract class Engine : IComponent
+	public class Engine : IComponent
 	{
         public double Overheat { get; set; } //engine overheating > 100 degrees
         public Enums.EEngine EngineType { get; set; }
-        public abstract void ConsumeFuel(Fuel f);
+		public virtual void ConsumeFuel(Fuel f) { }
 	}
 }
