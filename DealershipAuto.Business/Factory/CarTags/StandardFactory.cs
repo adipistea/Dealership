677b9e3276be26921d829e72683a7818d9adde56
Partoaca Marius <partoaca.marius@gmail.com>
2016-04-21 14:43:28 +0300

    Factory new

---
+++ DealershipAuto.Business/Factory/CarTags/StandardFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DealershipAuto.Business;
+using DealershipAuto.DealershipAuto.Business.CarTags;
+using DealershipAuto.Business.Car_Components;
+using DealershipAuto.Business.Car_Components.Engine_Adapter;
+using DealershipAuto.Business.Car_Prototype;
+
+namespace DealershipAuto.DealershipAuto.Business.Factory.CarTypes
+{
+	public class StandardFactory : CarFactory
+	{
+        ICar car {get; set;}
+
+		public override Car GetCar()
+		{
+            Car newCar = (Car) car.Clone();
+            newCar.Id = ++_iNumberOfCars;
+            return newCar;
+		}
+	}
+}
